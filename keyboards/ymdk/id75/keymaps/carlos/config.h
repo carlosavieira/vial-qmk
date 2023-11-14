2a858eb93ec4f232fdd3858b66c0bba41233e13f
Carlos Vieira <carlosadrianovieira@gmail.com>
2023-11-13 21:23:26 -0300

    add my keymap config for ymdk/id75

---
+++ keyboards/ymdk/id75/keymaps/carlos/config.h
@@ -0,0 +1,15 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+#pragma once
+
+#define VIAL_KEYBOARD_UID {0x32, 0x5D, 0xA4, 0xC4, 0xA5, 0x41, 0xB7, 0xA7}
+
+#define VIAL_UNLOCK_COMBO_ROWS {0, 1}
+#define VIAL_UNLOCK_COMBO_COLS {0, 1}
+
+#define DYNAMIC_KEYMAP_MACRO_COUNT 16
+#define VIAL_COMBO_ENTRIES 16               // number of combos used, each entry take 10 byte in EEPROM
+#define VIAL_TAP_DANCE_ENTRIES 16           // number of tap dances used, each entry take 10 byte in EEPROM
+#define VIAL_KEY_OVERRIDE_ENTRIES 16         // number of key override used, each entry take 10 byte in EEPROM
+
+#define DYNAMIC_KEYMAP_LAYER_COUNT 16
