a1aa6fa27d7bd55e0206e3b46b49aef925f9d5d9
Carlos Vieira <carlosadrianovieira@gmail.com>
2023-11-13 23:25:58 -0300

    add layer color per key

--- keyboards/ymdk/id75/keymaps/carlos/config.h
+++ keyboards/ymdk/id75/keymaps/carlos/config.h
@@ -2,14 +2,25 @@
 
 #pragma once
 
-#define VIAL_KEYBOARD_UID {0x32, 0x5D, 0xA4, 0xC4, 0xA5, 0x41, 0xB7, 0xA7}
+#define VIAL_KEYBOARD_UID \
+    { 0x32, 0x5D, 0xA4, 0xC4, 0xA5, 0x41, 0xB7, 0xA7 }
 
-#define VIAL_UNLOCK_COMBO_ROWS {0, 1}
-#define VIAL_UNLOCK_COMBO_COLS {0, 1}
+#define VIAL_UNLOCK_COMBO_ROWS \
+    { 0, 1 }
+#define VIAL_UNLOCK_COMBO_COLS \
+    { 0, 1 }
 
 #define DYNAMIC_KEYMAP_MACRO_COUNT 16
-#define VIAL_COMBO_ENTRIES 16               // number of combos used, each entry take 10 byte in EEPROM
-#define VIAL_TAP_DANCE_ENTRIES 16           // number of tap dances used, each entry take 10 byte in EEPROM
-#define VIAL_KEY_OVERRIDE_ENTRIES 16         // number of key override used, each entry take 10 byte in EEPROM
+#define VIAL_COMBO_ENTRIES 16        // number of combos used, each entry take 10 byte in EEPROM
+#define VIAL_TAP_DANCE_ENTRIES 16    // number of tap dances used, each entry take 10 byte in EEPROM
+#define VIAL_KEY_OVERRIDE_ENTRIES 16 // number of key override used, each entry take 10 byte in EEPROM
 
 #define DYNAMIC_KEYMAP_LAYER_COUNT 16
+
+// #define RGB_MATRIX_RAINBOW_INDICATORS
+#define RGB_MATRIX_IDLE_TIMEOUT 1000 * 30
+#define RGB_DISABLE_TIMEOUT 1000 * 60 * 30
+#define RGB_DISABLE_WHEN_USB_SUSPENDED
+
+#define RGB_MATRIX_STARTUP_HUE 90
+#define RGB_MATRIX_STARTUP_MODE RGB_MATRIX_SOLID_COLOR
--- keyboards/ymdk/id75/keymaps/carlos/keymap.c
+++ keyboards/ymdk/id75/keymaps/carlos/keymap.c
@@ -14,7 +14,104 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
+#include "action_layer.h"
+#include "lib/lib8tion/lib8tion.h"
+#include "rgb_matrix.h"
 #include QMK_KEYBOARD_H
+
+#define RGB_MATRIX_LAYER_HUE_DELTA 42.5f
+
+const uint8_t rgb_matrix_capslock_indicator_pos[2] = {2, 0};
+
+// HELPER METHODS
+
+HSV hsv_set_hue(HSV hsv, int hue) {
+    HSV shifted = {.h = hue, .s = hsv.s, .v = hsv.v};
+    return shifted;
+}
+
+HSV hsv_hue_shift(HSV hsv, int delta) {
+    return hsv_set_hue(hsv, (hsv.h + delta) % 255);
+}
+
+void rgb_matrix_set_hsv_color(int index, HSV hsv) {
+    RGB rgb = hsv_to_rgb(hsv);
+    rgb_matrix_set_color(index, rgb.r, rgb.g, rgb.b);
+}
+
+void rgb_matrix_set_indicator(int index) {
+#ifdef RGB_MATRIX_RAINBOW_INDICATORS
+    uint8_t time = scale16by8(g_rgb_timer, 16);
+    HSV     hsv  = rgb_matrix_config.hsv;
+    hsv.h        = time;
+    rgb_matrix_set_hsv_color(index, hsv);
+#else
+    rgb_matrix_set_hsv_color(index, hsv_hue_shift(rgb_matrix_config.hsv, 127));
+#endif
+}
+
+void rgb_matrix_set_layer_color(int index, int layer) {
+    rgb_matrix_set_hsv_color(index, hsv_hue_shift(rgb_matrix_config.hsv, layer * RGB_MATRIX_LAYER_HUE_DELTA));
+}
+
+int resolve_effective_kc_layer(keypos_t keypos, int layer) {
+    if (layer == 0) return 0;
+    if (IS_LAYER_OFF(layer)) return resolve_effective_kc_layer(keypos, layer - 1);
+
+    int kc = keymap_key_to_keycode(layer, keypos);
+    if (kc == KC_TRNS) {
+        return resolve_effective_kc_layer(keypos, layer - 1);
+    }
+    return layer;
+}
+
+// API HOOKS
+
+bool rgb_matrix_indicators_advanced_user(uint8_t led_min, uint8_t led_max) {
+    uint8_t layer         = get_highest_layer(layer_state);
+    uint8_t default_layer = get_highest_layer(default_layer_state);
+
+    for (uint8_t row = 0; row < MATRIX_ROWS; ++row) {
+        for (uint8_t col = 0; col < MATRIX_COLS; ++col) {
+            uint8_t index = g_led_config.matrix_co[row][col];
+
+            if (last_input_activity_elapsed() > (uint32_t)RGB_MATRIX_IDLE_TIMEOUT) {
+                // SLEEPING
+                // HSV      hsv  = rgb_matrix_config.hsv;
+                // uint16_t time = scale16by8((g_rgb_timer - last_input_activity_time()), 16);
+                // hsv.v         = scale8(abs8(sin8(time) - 128) * 2, hsv.v);
+                // RGB rgb       = hsv_to_rgb(hsv);
+                // rgb_matrix_set_color(index, rgb.r, rgb.g, rgb.b);
+                rgb_matrix_set_color(index, RGB_OFF);
+            } else {
+                //  DEFAULT LAYER
+                if (layer == default_layer) {
+                    // capslock
+                    if (row == rgb_matrix_capslock_indicator_pos[0] && col == rgb_matrix_capslock_indicator_pos[1]) {
+                        if (host_keyboard_led_state().caps_lock) {
+                            rgb_matrix_set_indicator(index);
+                        }
+                    }
+                } else {
+                    // SUBLAYER
+                    keypos_t keypos = {col, row};
+
+                    // off
+                    if (keymap_key_to_keycode(layer, keypos) == KC_NO) {
+                        rgb_matrix_set_color(index, RGB_OFF);
+                    } else {
+                        uint8_t kc_layer = resolve_effective_kc_layer(keypos, layer);
+
+                        if (kc_layer > 0) {
+                            rgb_matrix_set_layer_color(index, kc_layer);
+                        }
+                    }
+                }
+            }
+        }
+    }
+    return false;
+}
 
 // clang-format off
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
